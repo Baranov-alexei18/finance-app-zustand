ec60b54f82cb2490c56efd15c19244f6641b0b28
Alexey Baranov <65853601+Baranov-alexei18@users.noreply.github.com>
2025-03-29 23:59:43 +0300

    update linter and github action

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,7 @@ export default [
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ignores: ['dist'],
+    ignores: ['dist', 'node_modules'],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
